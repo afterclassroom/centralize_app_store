c09d12d14960914995364f4d6df2a572208993e3
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2015-03-07 04:27:52 +0100

    new code

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -54,46 +54,46 @@ $(document).ready(function() {
 		return false;
 	});
 
-});
 
-$(document).ready(function() {
 	$("a.single_image").fancybox({
 		padding: 4,
 	});
-});
 
-/***************** Nav Transformicon ******************/
 
-/* When user clicks the Icon */
-$(".nav-toggle").click(function() {
-	$(this).toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
-});
+	/***************** Nav Transformicon ******************/
 
-/* When user clicks a link */
-$(".overlay ul li a").click(function() {
-	$(".nav-toggle").toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
-});
+	/* When user clicks the Icon */
+	$(".nav-toggle").click(function() {
+		$(this).toggleClass("active");
+		$(".overlay-boxify").toggleClass("open");
+	});
 
-/* When user clicks outside */
-$(".overlay").click(function() {
-	$(".nav-toggle").toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
-});
+	/* When user clicks a link */
+	$(".overlay ul li a").click(function() {
+		$(".nav-toggle").toggleClass("active");
+		$(".overlay-boxify").toggleClass("open");
+	});
 
-/***************** Smooth Scrolling ******************/
+	/* When user clicks outside */
+	$(".overlay").click(function() {
+		$(".nav-toggle").toggleClass("active");
+		$(".overlay-boxify").toggleClass("open");
+	});
 
-$('a[href*=#]:not([href=#])').click(function() {
-	if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+	/***************** Smooth Scrolling ******************/
 
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		if (target.length) {
-			$('html,body').animate({
-				scrollTop: target.offset().top
-			}, 2000);
-			return false;
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 2000);
+				return false;
+			}
 		}
-	}
+	});
+
 });
